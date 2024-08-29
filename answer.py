16ceb30904277162584899af92e29fefcfa2f74f
mouad ettali <mettali@ekinox.io>
2024-08-29 16:33:31 +0200

    new: remove old code

--- answer.py
+++ answer.py
@@ -1,21 +1,8 @@
 def get_number_from_string(string: str)-> int:
-    l = 0
-    r = len(string) - 1 
-    while l != r:
-        if not string[l].isdigit():
-            l+=1
-        elif not string[r].isdigit():
-            r-=1
-        elif string[l].isdigit() and string[r].isdigit():
-            return int(string[l] + string[r])
-    return int(string[l] + string[r])
+    return 0
+
+
 
 
 with open('example.txt', 'r') as example:
     content = example.readlines()
-
-numbers = []
-for line in content:
-    numbers.append(get_number_from_string(line))
-
-print(sum(numbers))
